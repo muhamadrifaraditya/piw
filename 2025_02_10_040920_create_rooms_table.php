b5c88165f3a78254f3a3dcf37380884616992d2c
muhamadrifaraditya <141134428+muhamadrifaraditya@users.noreply.github.com>
2025-02-18 20:03:36 +0700

    Add files via upload

---
+++ 2025_02_10_040920_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kamar')->nullable();
+            $table->string('gambar')->nullable();
+            $table->string('deskripsi')->nullable();
+            $table->string('harga')->nullable();
+            $table->string('wifi')->default('ya');
+            $table->string('type_kamar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
